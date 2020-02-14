8273a645212fbf4991192522dd4fd342fb0e857c
TopaGeor <george.top@windowslive.com>
2020-02-14 12:49:01 +0200

    Add Fixture, and some migrations changes

---
+++ Crowdfund_TeamProject.Test/Crowdfund_TeamProjectFixure.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Crowdfund_TeamProject.Data;
+
+namespace Crowdfund_TeamProject.Test
+{
+    public class Crowdfund_TeamProjectFixure
+    {
+        public Crowdfund_TeamProjectDbContext DbContext { get; private set; }
+
+        public ILifetimeScope Container { get; private set; }
+
+        public Crowdfund_TeamProjectFixure()
+        {
+            Container = ServiceRegistrator.GetContainer().BeginLifetimeScope();
+            DbContext = Container.Resolve<Crowdfund_TeamProjectDbContext>();
+        }
+
+        public void Dispose()
+        {
+            DbContext.Dispose();
+            Container.Dispose();
+        }
+    }
+}
