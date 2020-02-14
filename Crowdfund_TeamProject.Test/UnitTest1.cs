ed70351015f966eb9aa19391f4da3df854fe7675
TopaGeor <george.top@windowslive.com>
2020-02-14 11:19:45 +0200

    Did it work ?

---
+++ Crowdfund_TeamProject.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Crowdfund_TeamProject.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
